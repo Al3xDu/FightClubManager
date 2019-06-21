e1aae143a2ef30e445851825bbd5b466880a12c4
DumaAlexandru <alex.duma123@gmail.com>
2019-06-21 18:22:22 +0300

    First basic structure of the game is functional.

--- team.py
+++ team.py
@@ -29,6 +29,17 @@ class Team:
         self.wins = 0
         self.losses = 0
 
+    def rating(self):
+        """
+        what is the rating of the team
+        """
+        rating = 0
+        for fighter in self.fighters:
+            rating += fighter.skill
+        return rating
+
+    def __str__(self):
+        return '{} {}'.format(self.name, self.rating())
 
 class Manager:
     """
@@ -48,8 +59,30 @@ class Game:
 
         self.home_team = home_team
         self.away_team = away_team
+
+        self.home_team_won = None
+
+        print('{} vs. {} '.format(self.home_team, self.away_team))
+
     def play(self):
-        return None
+        """
+        play the game and return the one who won
+        True means the home team won, False means the away team won
+        """
+        print('Match begins')
+
+        # insert match here
+
+        print('Match ends')
+
+        if self.home_team.rating() > self.away_team.rating():
+            print('{} wins'.format(self.home_team))
+            self.home_team_won = True
+        else:
+            print('{} wins'.format(self.away_team))
+            self.home_team_won = False
+        return True
+
 
 class League:
     """
@@ -68,13 +101,16 @@ class League:
         """
         play 10 rounds between all our teams
         """
+        print('Tournament begins.')
         for i in range(10):
             self.play_round()
+        print('Tournament ends.')
 
     def play_round(self):
         """
         play a round, which is 3 matches
         """
+        print('Round begins.')
         num_teams = len(self.teams)
         num_games = num_teams // 2
 
@@ -85,5 +121,26 @@ class League:
             away_team = random.choice(teams_to_play)
             teams_to_play.remove(away_team)
 
-            game = Game(self,home_team, away_team )
+            game = Game(self, home_team, away_team)
             game.play()
+            self.resolve_game(game)
+
+        print('Round ends.')
+
+        print('Ladder:')
+        #ladder status for the team
+        self.ladder()
+
+    def ladder(self):
+        for team in sorted(self.teams, key =lambda t: -t.wins):
+            print('{}: {} wins.'.format(team, team.wins))
+
+    def resolve_game(self, game):
+        if game.home_team_won:
+            # home team won
+            game.home_team.wins += 1
+            game.away_team.losses += 1
+        else:
+            # away team won
+            game.home_team.losses += 1
+            game.away_team.wins += 1
